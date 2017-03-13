81fd1b6c16cb726a83d4b051517eb82ff802ceae
Felix <blazingfelix@gmail.com>
2017-03-13 12:24:31 -0700

    Add validation before submit
    Implementation validation before submit, checks to see if all user
    registration information is correct before submitting.

--- javascript/login.js
+++ javascript/login.js
@@ -1,3 +1,8 @@
+var checkFormUser = false;
+var checkFormEmail = false;
+var checkFormPass1 = false;
+var checkFormPass2 = false;
+
 function $(id) {
     var element = document.getElementById(id);
     if (element == null)
@@ -8,8 +13,8 @@ function $(id) {
 function checkUserLogin(id) {
     var user = $(id).value;
     var text = "";
-    if(user.length == 0) {
-      text = "Please enter your username";
+    if (user.length == 0) {
+        text = "Please enter your username";
     }
     document.getElementById("userLoginWarn").innerHTML = text;
 }
@@ -17,8 +22,8 @@ function checkUserLogin(id) {
 function checkPassLogin(id) {
     var pass = $(id).value;
     var text = "";
-    if(pass.length == 0) {
-      text = "Please enter your password"
+    if (pass.length == 0) {
+        text = "Please enter your password"
     }
     document.getElementById("passLoginWarn").innerHTML = text;
 }
@@ -27,12 +32,14 @@ function checkPassLogin(id) {
 function checkEmailRegi(id) {
     var email = $(id).value;
     var text = "";
-    if(email.length != 0) {
-      if((email.indexOf('@') > -1) && (email.indexOf('.') > -1)) {
-          text = "";
-      } else {
-        text = "Invalid email type";
-      }
+    checkFormEmail = true;
+    if (email.length != 0) {
+        if ((email.indexOf('@') > -1) && (email.indexOf('.') > -1)) {
+            text = "";
+        } else {
+            text = "Invalid email type";
+            checkFormEmail = false;
+        }
     } else {
         text = "Please enter a new email";
     }
@@ -42,12 +49,14 @@ function checkEmailRegi(id) {
 function checkUserRegi(id) {
     var user = $(id).value;
     var text = "";
-    if(user.length != 0) {
-      if(user.length < 5) {
-        text = "Username must be longer than 5 characters";
-      }
+    checkFormUser = true;
+    if (user.length != 0) {
+        if (user.length < 5) {
+            text = "Username must be longer than 5 characters";
+            checkFormUser = false;
+        }
     } else {
-      text = "Please enter a new username";
+        text = "Please enter a new username";
     }
     document.getElementById("userRegiWarn").innerHTML = text;
 }
@@ -56,25 +65,38 @@ function checkUserRegi(id) {
 function checkPassRegi(id) {
     var pass = $(id).value;
     var text = "";
-    if(pass.length != 0) {
-      if(pass.length < 6) {
-        text = "Password must to be longer than 6 characters";
-      }
+    if (pass.length != 0) {
+        if (pass.length < 6) {
+            text = "Password must to be longer than 6 characters";
+        }
     } else {
-      text = "Please enter a new password";
+        text = "Please enter a new password";
     }
     document.getElementById("passRegiWarn2").innerHTML = text;
 }
 
 /* Checks to see if both passwords match */
 function checkPassRegiMatch() {
-   var password1 = document.getElementById('passRegister1');
-   var password2 = document.getElementById('passRegister2');
-   var text = "";
-   if(password1.value == password2.value) {
-     text = "";
-   } else {
-     text = "Passwords do not match.";
-   }
-   document.getElementById("passRegiWarn2").innerHTML = text;
+    var password1 = document.getElementById('passRegister1');
+    var password2 = document.getElementById('passRegister2');
+    var text = "";
+    if (password1.value == password2.value) {
+        text = "";
+        checkFormPass1 = true;
+        checkFormPass2 = true;
+    } else {
+        text = "Passwords do not match.";
+    }
+    document.getElementById("passRegiWarn3").innerHTML = text;
+}
+
+/* Validates all forms all filled in properly, client-side */
+function validation() {
+    var text = "";
+    var valid = false;
+    if (checkFormUser && checkFormEmail && checkFormPass1 && checkFormPass2) {
+    } else {
+    }
+    document.getElementById("regiError").innerHTML = text;
+    return valid;
 }
