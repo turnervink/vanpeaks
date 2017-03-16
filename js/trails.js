412adeb10b413550e3505ad60cdfbc98d5449a17
Noah Seltzer <noahseltzer@outlook.com>
2017-03-16 06:09:25 -0700

    updated Lynn Peak review and info

--- js/trails.js
+++ js/trails.js
@@ -3,3 +3,20 @@ $(document).ready(function(){
 		$('#review_form').toggle(750);
 	});
 })
+
+
+   function showPicture(){
+       var preview = document.querySelector("#img_display_frame"); //selects the query named img
+       var file    = document.querySelector("#filupload").files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
