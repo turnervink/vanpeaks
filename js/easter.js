38f93642bce928bc329e6e089b191f3d765ea121
Turner Vink <turner@turnervink.com>
2017-04-12 22:01:33 -0700

    Change nav hover effect

--- js/easter.js
+++ js/easter.js
@@ -1,14 +1,19 @@
-var clicks;
+// var clicks;
+//
+// function dab() {
+//   clicks++;
+//
+//   if (clicks >= 10) {
+//     $("#teamphoto").attr("src", "images/easteregg.jpg");
+//     $("#abouttitle").text("-About DabPeaks-");
+//   }
+// }
 
-function dab() {
-  clicks++;
-
-  if (clicks >= 10) {
-    $("#teamphoto").attr("src", "images/easteregg.jpg");
-    $("#abouttitle").text("-About DabPeaks-");
-  }
-}
+$("#teamphoto").hover(function() {
+  console.log("Hover");
+  this.attr("src", "images/easteregg.jpg");
+});
 
 $(document).ready(function() {
-    clicks = 0;
+    //clicks = 0;
 });
